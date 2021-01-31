74c551042cd304fc61aff5e4d9585ab46abb21e2
Nicolas Seinlet <nse@odoo.com>
2021-01-31 20:41:59 +0100

    Transmit signal through BLE add potentiometers for trims

--- anna_ir_receiver/anna_ir_receiver.ino
+++ anna_ir_receiver/anna_ir_receiver.ino
@@ -1,64 +1,114 @@
-// IR Transmitter ...
-#include <IRremote.h>
 #include <Servo.h>
+#include <ArduinoBLE.h>
+
+#define pwm_peripheral_name "PWM_Emitter"
+#define service_uuid "fd6db617-daf2-4502-823c-3d9ae8915948"
+#define pwmchannels_uuid "c9724225-18b5-4e34-81bc-0293b7cbddd4"
 
 int receiverPin=9;
-int val;
+//int val;
 int pot;
+int pot_trim;
 int servo_max_angle;
 unsigned long data;
-decode_results results;
-IRrecv irrecv(receiverPin);
 Servo myservo[4];
+int servo_ins[4];
+int servo_trims[4];
 int servo_vals[4];
+BLECharacteristic readingchan;
+uint32_t val;
 
 void setup() {
   Serial.begin(115200);
-  irrecv.enableIRIn();
-  irrecv.blink13(true);
-  myservo[0].attach(6); 
-  myservo[1].attach(5);
-  myservo[2].attach(4);
-  myservo[3].attach(3);
+
+  for (int i = 0; i < 4; i++) {
+    myservo[i].attach(i+3);
+  };
+  servo_ins[0]=A0;
+  servo_ins[1]=A1;
+  servo_ins[2]=A2;
+  servo_ins[3]=A3;
+  servo_trims[0]=A4;
+  servo_trims[1]=A5;
+  servo_trims[2]=A6;
+  servo_trims[3]=A7;
+  // begin initialization
+  if (!BLE.begin()) {
+    Serial.println("starting BLE failed!");
+    while (1);
+  }
+
+  Serial.println("BLE Central - Peripheral Explorer");
+
+  BLE.scanForName(pwm_peripheral_name);
 }
 
 void loop() {
-  delay(200);
-  if (irrecv.decode(&results)) {
-    if (results.decode_type == NEC) {
-      //Serial.print("NEC: ");
-    } else if (results.decode_type == SONY) {
-      //Sony is the servo value
-      //Serial.print("SONY: ");
-      val=( int ) results.value;
-    } else if (results.decode_type == RC5) {
-      //Serial.print("RC5: ");
-    } else if (results.decode_type == RC6) {
-      //Serial.print("RC6: ");
-    } else if (results.decode_type == UNKNOWN) {
-      //Serial.print("UNKNOWN: ");
+  // check if a peripheral has been discovered
+  BLEDevice peripheral = BLE.available();
+
+  if (peripheral) {
+    // see if peripheral is a PWM emitter
+    if (peripheral.localName() == "PWM_Emitter") {
+      // stop scanning
+      BLE.stopScan();
+      // turn on the LED to indicate the connection:
+      digitalWrite(LED_BUILTIN, HIGH);
+
+      explorerPeripheral(peripheral);
+      digitalWrite(LED_BUILTIN, LOW);
     }
-    //Serial.println(results.value, HEX);
-    
-    //Serial.println(val);
-    delay(200);
-    irrecv.resume(); // Receive the next value
-    
+    BLE.scanForName(pwm_peripheral_name);
   }
+};
 
-  //Servo 0 = input
-  delay(1);
-  servo_vals[0]=map(val, 0, 1023, 0, 180);
-  myservo[0].write(map(val, 0, 1023, 0, 180)); 
-  //Servo 1 =avg(input, potentiometer)
+void explorerPeripheral(BLEDevice peripheral) {
+  // connect to the peripheral
+  Serial.println("Connecting ...");
 
-  for (int i = 1; i < 4; i++) {
+  if (peripheral.connect()) {
+    Serial.println("Connected");
+  } else {
+    Serial.println("Failed to connect!");
+    return;
+  }
+  if (peripheral.connected()) {
+    // discover peripheral attributes
+    Serial.println("Discovering attributes ...");
+    if (peripheral.discoverAttributes()) {
+      Serial.println("Attributes discovered");
+    } else {
+      Serial.println("Attribute discovery failed!");
+      peripheral.disconnect();
+      return;
+    }
+
+    BLEService pwmService = peripheral.service(service_uuid);
+    if (pwmService) {
+      readingchan = pwmService.characteristic(pwmchannels_uuid);
+      while (peripheral.connected()) {
+        readingchan.readValue(val);
+        Serial.println(val);
+        move_servos();
+      };
+    };
+    peripheral.disconnect();
+
+  }
+  
+};
+
+void move_servos(){
+
+  for (int i = 0; i < 4; i++) {
       //Potentiometer control max steering
-      pot = analogRead(i);
+      pot = analogRead(servo_ins[i]);
+      pot_trim = analogRead(servo_trims[i]);
+      
       //Max angle is a map between pot values (0->1023) and servo angle values in a given direction (0->90) 
       servo_max_angle=map(pot, 0, 1023, 0, 90);
-      servo_vals[i]=map(val, 0, 1023, servo_max_angle, 180-servo_max_angle);
-      myservo[1].write(servo_vals[i]); 
+      servo_vals[i]=map(val+pot_trim-512, 0, 1023, servo_max_angle, 180-servo_max_angle);
+      myservo[i].write(servo_vals[i]); 
   }
   
   Serial.print(val);
@@ -67,4 +117,4 @@ void loop() {
      Serial.print(servo_vals[i]);
   }
   Serial.println();
-}
+};
--- anna_ir_transmitter/anna_ir_transmitter.ino
+++ anna_ir_transmitter/anna_ir_transmitter.ino
@@ -1,20 +1,72 @@
-// IR Transmitter ...
-#include <IRremote.h>
+#include <ArduinoBLE.h>
 
 int val;
+int last_val;
 unsigned long data;
-IRsend irsend;
+
+//Bluetooth part
+BLEService PWMService("fd6db617-daf2-4502-823c-3d9ae8915948");  // BLE PWM service
+BLEIntCharacteristic PWMchannelsCharacteristic("c9724225-18b5-4e34-81bc-0293b7cbddd4", BLERead | BLENotify);
 
 void setup()
 {
   val = 0;
+  last_val = 0;
+  Serial.begin(115200);
+
+    //Start bluetooth emitter
+  if (!BLE.begin()) {   // initialize BLE
+    Serial.println("starting BLE failed!");
+    while (1);
+  }
+
+  BLE.setLocalName("PWM_Emitter");  // Set name for connection
+  BLE.setAdvertisedService(PWMService); // Advertise service
+  PWMService.addCharacteristic(PWMchannelsCharacteristic); // Add characteristic to service
+  BLE.addService(PWMService); // Add service
+  PWMchannelsCharacteristic.setValue(512);
+
+  BLE.advertise();  // Start advertising
+  Serial.print("Peripheral device MAC: ");
+  Serial.println(BLE.address());
+  Serial.println("Waiting for connections...");
 }
 
 void loop() {   
-  val = analogRead(2);
-  data = ( unsigned long ) val;
-  delay(50);
-  //data=0xa90;
-  irsend.sendSony(data, 12);
-  delay(200);
+  read_input();
+  
+  Serial.println(val);
+  Serial.print("Peripheral device MAC: ");
+  Serial.println(BLE.address());
+
+  // listen for BLE peripherals to connect:
+  BLEDevice central = BLE.central();
+
+  // if a central is connected to the peripheral:
+  if (central) {
+    Serial.print("Connected to central: ");
+    // print the central's BT address:
+    Serial.println(central.address());
+    // turn on the LED to indicate the connection:
+    digitalWrite(LED_BUILTIN, HIGH);
+
+    while (central.connected()){
+      read_input();
+    };
+    // when the central disconnects, turn off the LED:
+    digitalWrite(LED_BUILTIN, LOW);
+    Serial.print("Disconnected from central: ");
+    Serial.println(central.address());
+  }
 }
+
+void read_input() {
+  //A2 is the trailer angle.
+  //A3 is the trim
+  val = analogRead(A2)+analogRead(A3)-512;
+  Serial.println(val);
+  if (last_val!=val){
+    PWMchannelsCharacteristic.writeValue(val);
+    last_val=val;
+  };
+};
